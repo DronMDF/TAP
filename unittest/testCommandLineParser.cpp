7f075a9b29ea1d1ad666660a507946539dddf8e0
Andrey Valyaev <dron.valyaev@gmail.com>
2010-02-09 15:28:51 +0300

    пытался вынести общую часть - но это оказалось проблематично

--- unittest/testCommandLineParser.cpp
+++ unittest/testCommandLineParser.cpp
@@ -18,6 +18,24 @@ BOOST_AUTO_TEST_SUITE(suiteCommandLineParser);
 
 static onullstream nullout;
 
+BOOST_AUTO_TEST_CASE(testHelp)
+{
+	const char *argv[] = { "xap", "help", };
+	CommandLineParser cmd(2, argv);
+	ostringstream out;
+	cmd.run(out);
+	BOOST_REQUIRE_EQUAL(out.str(), CommandLineParser::help);
+}
+
+struct testCommandLineParser : public CommandLineParser {
+	testCommandLineParser(int argc, const char **argv)
+		: CommandLineParser(argc, argv)
+	{};
+
+	using CommandLineParser::m_options;
+	using CommandLineParser::m_args;
+};
+
 BOOST_AUTO_TEST_CASE(testOptions)
 {
 	const string server = "test:1234";
@@ -30,10 +48,9 @@ BOOST_AUTO_TEST_CASE(testOptions)
 		"-k", key.c_str(),
 		"-n", num.c_str()
 	};
-	struct testCLP : public CommandLineParser {
-		testCLP(int argc, const char **argv) : CommandLineParser(argc, argv) {};
-		using CommandLineParser::m_options;
-	} cmd(sizeof(argv) / sizeof(argv[0]), argv);
+
+	testCommandLineParser cmd(sizeof(argv) / sizeof(argv[0]), argv);
+	
 	BOOST_REQUIRE_EQUAL(cmd.m_options["server"], server);
 	BOOST_REQUIRE_EQUAL(cmd.m_options["certfile"], cert);
 	BOOST_REQUIRE_EQUAL(cmd.m_options["keyfile"], key);
@@ -44,10 +61,7 @@ BOOST_AUTO_TEST_CASE(testServerComplete)
 {
 	const string server = "test";
 	const char *argv[] = { "xap", "-s", server.c_str() };
-	struct testCLP : public CommandLineParser {
-		testCLP(int argc, const char **argv) : CommandLineParser(argc, argv) {};
-		using CommandLineParser::m_options;
-	} cmd(sizeof(argv) / sizeof(argv[0]), argv);
+	testCommandLineParser cmd(sizeof(argv) / sizeof(argv[0]), argv);
 	BOOST_REQUIRE_EQUAL(cmd.m_options["server"], server + ":4433");
 }
 
@@ -55,27 +69,15 @@ BOOST_AUTO_TEST_CASE(testCommands)
 {
 	const string command = "load";
 	const char *argv[] = { "xap", "-n", "666", command.c_str() };
-	struct testCLP : public CommandLineParser {
-		testCLP(int argc, const char **argv) : CommandLineParser(argc, argv) {};
-		using CommandLineParser::m_args;
-	} cmd(sizeof(argv) / sizeof(argv[0]), argv);
+	testCommandLineParser cmd(sizeof(argv) / sizeof(argv[0]), argv);
 	BOOST_REQUIRE_EQUAL(cmd.m_args.front(), command);
-}
-
-BOOST_AUTO_TEST_CASE(testHelp)
-{
-	const char *argv[] = { "xap", "help", };
-	CommandLineParser cmd(2, argv);
-	ostringstream out;
-	cmd.run(out);
-	BOOST_REQUIRE_EQUAL(out.str(), CommandLineParser::help);
 }
 
 BOOST_AUTO_TEST_CASE(testLoadCreateHonest)
 {
 	const char *argv[] = { "xap", "load" };
-	struct testCLP : public CommandLineParser, private visit_mock {
-		testCLP(int argc, const char **argv) : CommandLineParser(argc, argv) {};
+	struct inlineCommandLineParser : public testCommandLineParser, private visit_mock {
+		inlineCommandLineParser(int argc, const char **argv) : testCommandLineParser(argc, argv) {};
 		void manageClients(client_creator_t creator) const {
 			visit();
 			Client *honest = creator();
