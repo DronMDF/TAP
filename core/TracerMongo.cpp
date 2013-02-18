8f6f92bddbf68be6fb7b3239d732c203ba573302
Andrey Valyaev <dron.valyaev@gmail.com>
2013-02-18 15:22:59 +0400

    fix x64 incompatible type

--- core/TracerMongo.cpp
+++ core/TracerMongo.cpp
@@ -29,7 +29,8 @@ void TracerMongo::trace(unsigned id, const string &msg) const
 {
 	BSONObjBuilder bson;
 	const auto time = high_resolution_clock::now();
-	bson.append("timestamp", duration_cast<milliseconds>(time.time_since_epoch()).count());
+	const long long timestamp = duration_cast<milliseconds>(time.time_since_epoch()).count();
+	bson.append("timestamp", timestamp);
 	bson.append("cid", id);
 	bson.append("message", msg);
 	client->insert("test.tap", bson.obj());
@@ -39,7 +40,8 @@ void TracerMongo::trace(unsigned id, const std::string &key, unsigned value) con
 {
 	BSONObjBuilder bson;
 	const auto time = high_resolution_clock::now();
-	bson.append("timestamp", duration_cast<milliseconds>(time.time_since_epoch()).count());
+	const long long timestamp = duration_cast<milliseconds>(time.time_since_epoch()).count();
+	bson.append("timestamp", timestamp);
 	bson.append("cid", id);
 	bson.append("name", key);
 	bson.append("value", value);
