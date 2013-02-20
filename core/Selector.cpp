95293ac4b3dcff2203a9ec07c42dd465471eeade
Andrey Valyaev <dron.valyaev@gmail.com>
2013-02-20 23:38:44 +0400

    Selector implements socket manage

---
+++ core/Selector.cpp
@@ -0,0 +1,34 @@
+
+#include "Selector.h"
+
+#include <stdexcept>
+#include "Socket.h"
+
+using namespace std;
+
+Selector::Selector()
+	: sockets()
+{
+}
+
+Selector::~Selector()
+{
+}
+
+void Selector::addSocket(const std::shared_ptr<Socket> &socket)
+{
+	const int fd = socket->getDescriptor();
+	if (sockets.find(fd) != sockets.end()) {
+		throw runtime_error("Descriptor already in use");
+	}
+	sockets[fd] = socket;
+}
+
+void Selector::removeSocket(const std::shared_ptr<Socket> &socket)
+{
+	const int fd = socket->getDescriptor();
+	if (sockets.find(fd) == sockets.end()) {
+		throw runtime_error("Descriptor not in use");
+	}
+	sockets.erase(fd);
+}
--- core/Selector.h
+++ core/Selector.h
@@ -1,16 +1,21 @@
 
 #pragma once
+#include <map>
 #include <memory>
 
 class Socket;
 
 class Selector {
 public:
-	virtual ~Selector() {};
+	Selector();
+	virtual ~Selector();
 
-	virtual void addSocket(const std::shared_ptr<Socket> &socket) = 0;
-	virtual void removeSocket(const std::shared_ptr<Socket> &socket) = 0;
+	virtual void addSocket(const std::shared_ptr<Socket> &socket);
+	virtual void removeSocket(const std::shared_ptr<Socket> &socket);
 
 	virtual void proceed() = 0;
+
+protected:
+	std::map<int, std::shared_ptr<Socket>> sockets;
 };
 
